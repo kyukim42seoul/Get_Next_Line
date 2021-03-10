5bbaff37430b5d263a4466c364b167a742874553
kyukim <kyukim@student.42seoul.kr>
2021-03-10 12:02:33 +0900

    SAME with Prev commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kyukim <kyukim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 01:26:51 by kyukim            #+#    #+#             */
-/*   Updated: 2021/03/10 11:41:21 by kyukim           ###   ########.fr       */
+/*   Updated: 2021/03/10 12:01:11 by kyukim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,7 @@ int	set_backup(int fd, char *buf, char **backup, char **is_next_line)
 	{
 		read_len = read(fd, buf, BUFFER_SIZE);
 		if (read_len < 0)
-		{
-//			free(buf);
 			return (-1);
-		}
 		else if (read_len == 0)
 			break ;
 		else
@@ -39,7 +36,6 @@ int	set_backup(int fd, char *buf, char **backup, char **is_next_line)
 			*backup = temp;
 		}
 	}
-//	free(buf);
 	return (1);
 }
 
